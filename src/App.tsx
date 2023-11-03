494043eb10f7d829af933ff10fa24a7022e5c2de
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-11-03 15:19:10 +0700

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import Game from "./components/Game";
 import { useGame } from "./context/GameContext";
 import GameWin from "./components/GameWin";
 import GameIntro from "./components/GameIntro";
+import bg from "./assets/bg.svg";
 
 function App() {
   const { gameOn, gameWin } = useGame();
 
   return (
-    <main className="h-screen bg-purple-400 bg-graddient-to-r from-purple-600 via-pink-300 to-purple-600 flex flex-col items-center justify-center">
+    <main
+      style={{ background: `url("${bg}")` }}
+      className="h-screen bg-purple-400 bg-graddient-to-r from-purple-600 via-pink-300 to-purple-600 flex flex-col items-center justify-center"
+    >
       {gameWin ? (
         <GameWin />
       ) : gameOn ? (
