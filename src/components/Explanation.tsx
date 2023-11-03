3517578e6df8bb227614c7a5057497a6a777edd6
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-11-03 11:03:33 +0700

    fix shifting button labels

--- src/components/Explanation.tsx
+++ src/components/Explanation.tsx
@@ -1,6 +1,7 @@
 import { useGame } from "../context/GameContext";
 import fullLogo from "../assets/full-logo.svg";
 import { levels } from "../lib/game-values";
+import { useEffect, useState } from "react";
 
 export default function Explanation({ explanation }: { explanation: string }) {
   const {
@@ -12,10 +13,18 @@ export default function Explanation({ explanation }: { explanation: string }) {
     setCurrentChoice,
   } = useGame();
 
+  const [isCorrect, setIsCorrect] = useState(false);
+
   const isChoiceCorrect =
     currentChoice !== null &&
     levels[currentLevel].questions[currentQuestion].choices[currentChoice]
       .isCorrect;
+
+  useEffect(() => {
+    if (isChoiceCorrect) {
+      setIsCorrect(true);
+    }
+  }, [isChoiceCorrect]);
   return (
     <div className="text-center">
       {/* <img src={fullLogo} /> */}
@@ -25,14 +34,12 @@ export default function Explanation({ explanation }: { explanation: string }) {
       <div className="text-5xl text-white tracking-tighter mb-8">
         {explanation}
       </div>
-      {isChoiceCorrect ? (
+      {isChoiceCorrect || isCorrect ? (
         <button
           className="bg-purple-900 text-white px-4 py-2 rounded-md hover:bg-purple-950 font-bold"
           onClick={() => {
             nextQuestion();
-            setTimeout(() => {
-              setCurrentChoice(null);
-            }, 1000);
+            setCurrentChoice(null);
           }}
         >
           Next Question
@@ -42,7 +49,6 @@ export default function Explanation({ explanation }: { explanation: string }) {
           className="bg-purple-900 text-white px-4 py-2 rounded-md hover:bg-purple-950 font-bold"
           onClick={() => {
             setExplainMode(false);
-
             setCurrentChoice(null);
           }}
         >
